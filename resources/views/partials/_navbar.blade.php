2f7aa5c73961c8be06554707c4295b00d3b00261
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-13 10:16:21 -0600

    removed search bar from navbar for now.

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -39,18 +39,6 @@
                 </div>
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-0 md:pr-0">
 
-                    <div class="relative">
-                        <input type="text"
-                               class="pl-8 border focus:outline-none focus:border-black md:w-64 rounded-full px-3 py-1"
-                               placeholder="Search...">
-                        <div class="absolute top-0 flex items-center h-full ml-2">
-                            <svg id="Layer_1" class="fill-current text-gray-400 w-4" enable-background="new 0 0 128 128"
-                                 viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
-                                <path id="Search"
-                                      d="m118.828 113.172-29.036-29.037c6.366-7.633 10.208-17.441 10.208-28.135 0-24.262-19.738-44-44-44s-44 19.738-44 44 19.738 44 44 44c10.694 0 20.502-3.842 28.135-10.208l29.037 29.037c.781.781 1.805 1.172 2.828 1.172s2.047-.391 2.828-1.172c1.563-1.563 1.563-4.095 0-5.657zm-98.828-57.172c0-19.85 16.148-36 36-36s36 16.15 36 36-16.148 36-36 36-36-16.15-36-36z"/>
-                            </svg>
-                        </div>
-                    </div>
                     <!-- Profile dropdown -->
                     <dropdown-component>
                         <template v-slot:toggle>
