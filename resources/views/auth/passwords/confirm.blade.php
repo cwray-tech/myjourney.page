c86db756d0d16f6f12419539cdf60789e48408b5
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-13 19:15:53 -0600

    updated auth views and run npm production

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -3,49 +3,39 @@
     Confirm your Password
 @endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+    <section class="py-40 min-h-screen justify-center flex flex-col">
+        <div class="container px-8 md:flex md:flex-row-reverse md:align-items-center mx-auto">
+            <img class="md:w-1/4 w-1/2" src="/images/devjourney.svg" alt="Dev journey">
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+            <div class="lg:w-3/4 md:pr-16 md:pt-0 pt-5">
+                <h1 class="text-5xl">Confirm your password to continue.</h1>
+                <form method="POST" class="lg:w-3/4 mt-6" action="{{ route('password.confirm') }}">
+                    @csrf
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
+                    <label for="password">{{ __('Password') }}</label>
+                    <input id="password" type="password" placeholder="***********"
+                           class="input @error('password') is-invalid @enderror" name="password" required
+                           autocomplete="current-password">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                    @enderror
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
+                    <div class="flex">
+                        <button type="submit" class="btn btn-primary mr-3">
+                            {{ __('Confirm Password') }}
+                        </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,47 +3,34 @@
     Reset your Password
 @endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <section class="py-40 min-h-screen justify-center flex flex-col">
+        <div class="container px-8 md:flex md:flex-row-reverse md:align-items-center mx-auto">
+            <img class="md:w-1/4 w-1/2" src="/images/devjourney.svg" alt="Dev journey">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="lg:w-3/4 md:pr-16 md:pt-0 pt-5">
+                <h1 class="text-5xl">Reset your password.</h1>
+                <form method="POST" class="lg:w-3/4 mt-6" action="{{ route('password.email') }}">
+                    @csrf
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                    <label for="email">{{ __('E-Mail Address') }}</label>
+                    <input id="email" type="email" placeholder="youremail@website.com"
+                           class="input @error('email') is-invalid @enderror" name="email" required
+                           autocomplete="current-email" value="{{ old('email') }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                    @enderror
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="flex">
+                        <button type="submit" class="btn btn-primary mr-3">
+                            {{ __('Send Password Reset Link') }}
+                        </button>
+
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
