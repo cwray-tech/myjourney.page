43def7ba3bb19a7323dbd16c0017d6e9f73f313b
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-14 10:42:27 -0600

    updated footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,9 +1,10 @@
 <footer>
-    <div class="container flex justify-between mx-auto px-4 py-4">
-        <div class="mr-auto p-2">Created by Chris, at <a href="https://solmediaco.com" class="underline">Soltech</a>. All rights reserved.</div>
-        <div class="ml-auto flex">
-            <a href="https://github.com/cwray-tech" class="underline p-2">Github</a>
-            <a href="https://www.linkedin.com/in/christopher-wray/" class="underline p-2 ml-2">LinkedIn</a>
+    <div class="container md:flex justify-between mx-auto px-4 py-4">
+        <div class="mr-auto flex">
+            <a href="/" class="underline p-2">Home</a>
+            <a href="/journeys" class="underline p-2 ml-2">Journeys</a>
+            <a href="/about" class="underline p-2 ml-2">About</a>
         </div>
+        <div class="ml-auto p-2">Created by Chris, at <a href="https://solmediaco.com" class="underline">Soltech</a>. All rights reserved.</div>
     </div>
 </footer>
