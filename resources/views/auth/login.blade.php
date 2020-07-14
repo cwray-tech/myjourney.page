0581e17e5b644533197d18a9caa0114afacf1fd2
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-14 10:55:43 -0600

    worked on Footer more

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     Login
 @endsection
 @section('content')
-    <section class="py-40 min-h-screen justify-center flex flex-col">
+    <section class="py-40 min-h-screen flex flex-col justify-center items-center ">
         <div class="container px-8 md:flex md:flex-row-reverse md:align-items-center mx-auto">
             <img class="md:w-1/4 w-1/2" src="/images/devjourney.svg" alt="Dev journey">
 
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,10 +1,10 @@
 <footer>
-    <div class="container md:flex justify-between mx-auto px-4 py-4">
-        <div class="mr-auto flex">
+    <div class="container w-full text-center justify-between mx-auto p-4">
+        <div class="flex items-center justify-center">
             <a href="/" class="underline p-2">Home</a>
             <a href="/journeys" class="underline p-2 ml-2">Journeys</a>
             <a href="/about" class="underline p-2 ml-2">About</a>
         </div>
-        <div class="ml-auto p-2">Created by Chris, at <a href="https://solmediaco.com" class="underline">Soltech</a>. All rights reserved.</div>
+        <div class="p-2">Created by Chris, at <a href="https://solmediaco.com" class="underline">Soltech</a>. All rights reserved.</div>
     </div>
 </footer>
