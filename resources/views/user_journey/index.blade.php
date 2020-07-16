fcde3426fe81a191226ae7581bc0b7d40e749086
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-16 11:01:07 -0600

    added delete journey route and button

--- resources/views/user_journey/index.blade.php
+++ resources/views/user_journey/index.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
     <section class="py-40">
-        <div class="container max-w-screen-lg px-6 mx-auto">
+        <div class="container px-6 mx-auto">
             <h1 class="text-5xl mb-3">Your Journeys</h1>
             <p>View the Journeys you've created.</p>
             <div class="mt-6">
@@ -16,12 +16,17 @@
                        <div class="pl-6">
                            <h2 class="text-4xl mb-3">{{ $journey->title }}</h2>
                            <a href="{{route('journeys.show', $journey->slug)}}" class="underline">View Public Journey</a>
+                           <form method="post" action="{{ route('journeys.destroy') }}">
+                               @method('delete')
+                               @csrf
+                               <button type="submit" class="btn btn-danger">Delete Journey</button>
+                           </form>
                        </div>
 
                     </div>
                 @empty
-                    <div class="rounded-md bg-white shadow-sm border border-black p-5">
-                        <h2 class="text-4xl mb-3">Bummer, you haven't created any journeys yet. Want to add one?</h2>
+                    <div class="rounded-md bg-white shadow-sm border border-black p-6 pb-8">
+                        <h2 class="text-4xl mb-6">Bummer, you haven't created any journeys yet. Want to add one?</h2>
                         <a class="btn btn-cta" href="{{ route('journeys.create') }}">Write a Journey</a>
                     </div>
                 @endforelse
