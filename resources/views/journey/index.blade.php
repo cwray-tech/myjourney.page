7904dc8d34247079a0b333e1148316df8d542f4b
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-18 09:42:24 -0600

    removed journeys from index

--- resources/views/journey/index.blade.php
+++ resources/views/journey/index.blade.php
@@ -6,13 +6,7 @@
     <section class="py-40">
         <div class="container px-6 mx-auto">
             <h1 class="text-5xl mb-3">Journeys</h1>
-            @forelse($journeys as $journey)
-                <a href="/journeys/{{$journey->slug}}">
-                    <h2>{{$journey->title}}</h2>
-                </a>
-                @empty
             Bummer. We don't have any journeys here to share yet, but come back soon to start reading journeys, or <a href="/register" class="underline"> create your own account</a> to start writing your journeys!
-            @endforelse
         </div>
     </section>
 @endsection
