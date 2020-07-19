c6d52004d52c685cfc01a563689d7655db236849
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-18 21:26:26 -0600

    added v-cloak to nav x

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -16,7 +16,7 @@
                                   d="M4 6h16M4 12h16M4 18h16"/>
                         </svg>
                         <!-- Icon when menu is open. -->
-                        <svg v-else class="block h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg v-else v-cloak class="block h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                   d="M6 18L18 6M6 6l12 12"/>
                         </svg>
