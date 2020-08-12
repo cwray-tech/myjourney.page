119f9a8fde326b31d87e13681d2bf9bd204f9f1d
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-08-12 14:27:14 -0600

    updated user journeys view

--- resources/views/user_journey/index.blade.php
+++ resources/views/user_journey/index.blade.php
@@ -17,7 +17,7 @@
                     <div class="rounded-md  shadow-sm border p-4 mb-6">
                         <div class="w-full lg:flex items-start justify-between">
                             @if($journey->picture)
-                                <div class="h-64  flex justify-center items-center">
+                                <div class="h-64 flex justify-center items-center w-1/3">
                                     <img src="{{$journey->picture_path}}" class="w-full h-full object-cover rounded">
                                 </div>
                             @endif
@@ -25,7 +25,7 @@
                                 <h2 class="text-4xl mb-3">{{ $journey->title }}</h2>
                                 <p>{{ $journey->introduction }}</p>
                             </div>
-                            <div class="lg:w-2/5 p-4 flex lg:flex-col lg:items-end  justify-start flex-wrap space-x-4">
+                            <div class="p-4 flex lg:flex-col lg:items-end  justify-start flex-wrap space-x-4">
                                 <div class="w-full flex items-center lg:justify-end flex-wrap space-x-4">
                                     <a target="_blank" href="{{route('journeys.show', $journey->slug)}}"
                                        class="icon-button my-2"><img class="w-4 mr-2" src="/images/external.svg"> View</a>
