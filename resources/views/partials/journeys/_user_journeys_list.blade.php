8782d80e36606d0c04e960a2dee595d644ecfa59
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-08-19 20:42:24 -0600

    updated no journey div

--- resources/views/partials/journeys/_user_journeys_list.blade.php
+++ resources/views/partials/journeys/_user_journeys_list.blade.php
@@ -1,7 +1,7 @@
 @forelse($journeys as $journey)
         @include('partials.journeys._user_journey_preview')
 @empty
-    <div class="rounded-md flex flex-col justify-start bg-white shadow-sm shadow p-6 pb-8">
+    <div class="rounded-md flex flex-col justify-start bg-white border p-6 pb-8">
         <h2 class="text-3xl font-bold mb-6">Bummer, you haven't created any journeys yet. Want to add one?</h2>
         <a class="btn btn-cta mr-auto" href="{{ route('journeys.create') }}">Write a Journey</a>
     </div>
