657273624fe5d816eb15d6dcda5a6a1acf43388a
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-09-09 16:18:41 -0600

    updated homepage text

--- resources/views/static_pages/index.blade.php
+++ resources/views/static_pages/index.blade.php
@@ -14,7 +14,7 @@
                     <span class="text-yellow-500">Matters.</span>
                 </h1>
                 <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-                    MyJourney is the place where you can write and share your journeys, and make a difference in a life.
+                    The place where you can write and share your journeys.
                 </p>
                 <div class="mt-10 flex flex-col sm:flex-row justify-center lg:justify-start">
                     <div class="rounded-md shadow">
@@ -53,7 +53,7 @@
                         In bite sized steps.
                     </h3>
                     <p class="mt-3 text-lg leading-7 text-gray-500">
-                        Add and remove steps in your journey as you progress through it, and let others know about the life events that made you who you are today.
+                        Share the life events that made up your journey.
                     </p>
 
                     {{-- <ul class="mt-10">
@@ -128,7 +128,7 @@
                             Creating empathy.
                         </h3>
                         <p class="mt-3 text-lg leading-7 text-gray-500">
-                            Your journey can be anything because what matters is how we share our journeys so we can help others on theirs.
+                            Write your journey so you can help others on theirs.
                         </p>
                         {{--
                         <ul class="mt-10">
@@ -184,7 +184,7 @@
             <h2 class="text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
                 Change a life.
                 <br>
-                Share a journey.
+                Write a journey.
             </h2>
             <div class="mt-8 flex justify-center">
                 <div class="inline-flex rounded-md shadow">
