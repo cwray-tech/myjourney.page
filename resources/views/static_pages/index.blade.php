159c7ebd3bd4e5cab80c6c33a46c2fc7c98fc08e
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-09-09 16:20:57 -0600

    updated homepage text

--- resources/views/static_pages/index.blade.php
+++ resources/views/static_pages/index.blade.php
@@ -14,7 +14,7 @@
                     <span class="text-yellow-500">Matters.</span>
                 </h1>
                 <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-                    The place where you can write and share your journeys.
+                    This is the place where you can write and share it.
                 </p>
                 <div class="mt-10 flex flex-col sm:flex-row justify-center lg:justify-start">
                     <div class="rounded-md shadow">
